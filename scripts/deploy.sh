554e969776cb0fe8abfb2451634f78b2372dbf35
이영선 <leedrkr323@konkuk.ac.kr>
2024-01-18 14:25:19 +0900

    [fix] deploy.sh PROJECT_NAME 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=Springboot-AWS-Webservice
+PROJECT_NAME=springboot-aws-webservice
 
 echo ">> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">> 현재 구동 중인 애플리케이션 pid 확인"
 # CURRENT_PID=$(pgrep -f $PROJECT_NAME)
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "   현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -20,7 +20,7 @@ else
 fi
 
 echo ">> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.java | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo ">> JAR Name : $JAR_NAME"
 
 echo ">> $JAR_NAME 에 실행 권한 추가"
