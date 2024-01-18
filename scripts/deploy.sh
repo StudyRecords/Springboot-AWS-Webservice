80a3a01a2230ad27626c4d85790f21a80f929add
이영선 <leedrkr323@konkuk.ac.kr>
2024-01-18 13:48:54 +0900

    [fix] nohup.out 삭제 후 재배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">> 현재 구동 중인 애플리케이션 pid 확인"
 # CURRENT_PID=$(pgrep -f $PROJECT_NAME)
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
 echo "   현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -20,7 +20,7 @@ else
 fi
 
 echo ">> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.java | tail -n 1)
 echo ">> JAR Name : $JAR_NAME"
 
 echo ">> $JAR_NAME 에 실행 권한 추가"
