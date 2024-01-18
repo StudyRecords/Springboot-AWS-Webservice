f72f59662231557eb1e7e5db8fe6e8bc1cd6d51b
이영선 <leedrkr323@konkuk.ac.kr>
2024-01-18 18:56:29 +0900

    [fix] appspec.yml 오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -29,7 +29,9 @@ function find_idle_port()
     IDLE_PROFILE=$(find_idle_profile)
 
     if [${IDLE_PROFILE} == real1]
-    then echo "8081"
-    else echo "8082"
+    then
+        echo "8081"
+    else
+        echo "8082"
     fi
 }
